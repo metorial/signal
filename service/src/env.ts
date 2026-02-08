b3858c1f1d01ff4b1ee1618d5aeca4606e3ee9c9
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-08 17:19:22 +0100

    Build env URLs from process.env only

--- service/src/env.ts
+++ service/src/env.ts
@@ -3,23 +3,12 @@ import { v } from '@lowerdeck/validation';
 
 export let env = createValidatedEnv({
   service: {
-    REDIS_URL: v.optional(v.string()),
-    DATABASE_URL: v.optional(v.string())
+    REDIS_URL: v.string(),
+    DATABASE_URL: v.string()
   },
 
   storage: {
     OBJECT_STORAGE_URL: v.string(),
     LOGS_BUCKET_NAME: v.string()
-  },
-
-  DATABASE_USERNAME: v.optional(v.string()),
-  DATABASE_PASSWORD: v.optional(v.string()),
-  DATABASE_HOST: v.optional(v.string()),
-  DATABASE_PORT: v.optional(v.string()),
-  DATABASE_NAME: v.optional(v.string()),
-
-  REDIS_AUTH_TOKEN: v.optional(v.string()),
-  REDIS_HOST: v.optional(v.string()),
-  REDIS_PORT: v.optional(v.string()),
-  REDIS_TLS: v.optional(v.string())
+  }
 });
--- service/src/init.ts
+++ service/src/init.ts
@@ -1,35 +1,29 @@
-import { env } from './env';
-
 if (!process.env.DATABASE_URL) {
   if (
-    !env.DATABASE_USERNAME ||
-    !env.DATABASE_PASSWORD ||
-    !env.DATABASE_HOST ||
-    !env.DATABASE_PORT ||
-    !env.DATABASE_NAME
+    !process.env.DATABASE_USERNAME ||
+    !process.env.DATABASE_PASSWORD ||
+    !process.env.DATABASE_HOST ||
+    !process.env.DATABASE_PORT ||
+    !process.env.DATABASE_NAME
   ) {
     throw new Error(
       'DATABASE_URL is not set and database component env vars are missing'
     );
   }
 
-  process.env.DATABASE_URL = `postgres://${env.DATABASE_USERNAME}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/${env.DATABASE_NAME}?schema=public&sslmode=no-verify&connection_limit=20`;
+  process.env.DATABASE_URL = `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?schema=public&sslmode=no-verify&connection_limit=20`;
 }
 
 if (!process.env.REDIS_URL) {
-  if (!env.REDIS_AUTH_TOKEN || !env.REDIS_HOST || !env.REDIS_PORT) {
+  if (
+    !process.env.REDIS_AUTH_TOKEN ||
+    !process.env.REDIS_HOST ||
+    !process.env.REDIS_PORT
+  ) {
     throw new Error('REDIS_URL is not set and redis component env vars are missing');
   }
 
   process.env.REDIS_URL = `${
     process.env.REDIS_TLS == 'true' ? 'rediss' : 'redis'
-  }://:${env.REDIS_AUTH_TOKEN}@${env.REDIS_HOST}:${env.REDIS_PORT}/0`;
-}
-
-if (!env.service.DATABASE_URL) {
-  env.service.DATABASE_URL = process.env.DATABASE_URL;
-}
-
-if (!env.service.REDIS_URL) {
-  env.service.REDIS_URL = process.env.REDIS_URL;
+  }://:${process.env.REDIS_AUTH_TOKEN}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`;
 }
