2fbd114a9f0240662524a47d73b7c702d4c4243b
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-08 17:52:20 +0100

    Avoid top-level await for ncc

--- service/src/server.ts
+++ service/src/server.ts
@@ -1,4 +1,11 @@
-await import('./init');
-await import('./instrument');
-await import('./endpoints');
-await import('./worker');
+async function main() {
+  await import('./init');
+  await import('./instrument');
+  await import('./endpoints');
+  await import('./worker');
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
